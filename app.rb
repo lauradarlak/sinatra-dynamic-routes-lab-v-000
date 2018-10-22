168097b4ead5cd4b7decd5d9104d44e15d4a2d55
Laura Darlak <laura@polyporedesign.com>
2018-10-22 20:04:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,9 +30,7 @@ class App < Sinatra::Base
         params[:number1].to_i * params[:number2].to_i
     elsif params[:operation].include(divide)
         params[:number1].to_i / params[:number2].to_i
-      end
-
-
+    end
   end
 
 end
