462a96708d6ff01e11724f59e7d4f3519d049fe5
Laura Darlak <laura@polyporedesign.com>
2018-10-22 19:42:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
 
   get '/say/:number/:phrase' do
     @times = params[:number].to_i
-    return params[:phrase] * @times
+    params[:phrase] * @times
     # @times.times {puts params[:phrase]}
 
   end
